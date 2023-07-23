b6b1d9252cbe3e56c938cc5945f33fc43ed71494
R9_ <neamtrares@naver.com>
2023-07-23 21:32:10 +0300

    Level 0 Done

--- Game/Level0.gd
+++ Game/Level0.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
+var level_end = -1223
 var r_preload = preload("res://Player/Rocket/Rocket.tscn")
 
 func _input(event):
 	if event.is_action_pressed("rocket"):
 		var r = r_preload.instantiate()
+		r.position.x = level_end
 		r.position.y = $Player.position.y
 		#r.look_at($Player.position)
 		add_child(r)
 
 func _process(delta):
-	$Bar.value = $Player.health
+	$Camera2D.position.x = min($Player.position.x-100, 0)
+	$Camera2D/Bar.value = $Player.health
+
+func _on_floor_body_entered(body):
+	if body.name == "Player":
+		body.health = 0
+
+func _on_exit_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://UI/Win.tscn")
--- Game/Player/Rocket/Rocket.gd
+++ Game/Player/Rocket/Rocket.gd
@@ -1,6 +1,17 @@
-extends CharacterBody2D
-var speed = 3
+extends Area2D
+var speed = 10
 
 func _process(delta):
-	velocity.x += speed
-	move_and_slide()
+	position.x += speed
+
+func _on_area_2d_area_entered(area):
+	if area.name == "Enemy":
+		area.queue_free()
+		queue_free()
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Enemy":
+		body.queue_free()
+		queue_free()
+	if body.name == "Player": #or body.name == "Platforms":
+		queue_free()
